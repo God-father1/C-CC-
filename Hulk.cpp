23da3679173c06ed36023f0258b0f24a865380e5
God's_father <58758331+God-father1@users.noreply.github.com>
2020-04-02 21:18:16 +0530

    Hulk problem solution

---
+++ Hulk.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+  int a;
+  cin>>a;
+  for(int i=1; i<a ;i++)
+  {
+       if(i%2==0){
+          
+           cout<<"I love that ";
+       }
+       else
+        cout<<"I hate that ";
+  }
+  if(a%2==0){
+       cout<<"I love it ";    
+       }
+       else
+       cout<<"I hate it ";
+       
+  return 0;
+}
