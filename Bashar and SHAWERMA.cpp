7ba5c9b892d882ab5d38a505ebb990dc40367f84
God's_father <58758331+God-father1@users.noreply.github.com>
2020-04-12 19:55:59 +0530

    codeforces first comp....

---
+++ Bashar and SHAWERMA.cpp
@@ -0,0 +1,15 @@
+/*A. Bashar and SHAWERMA!
+time limit per test1.5 seconds
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output*/
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+  int a;
+  cin>>a;
+  cout<<a*2;
+  
+}
