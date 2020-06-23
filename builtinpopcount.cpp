40dd769645fdb71df24c6e7d2b9f365801d29b47
God's_Father <balaharode@gmail.com>
2020-06-23 23:21:46 +0530

    Count the number of set bits in a number

---
+++ builtinpopcount.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,count=0;
+	    cin>>n;
+	    cout<<__builtin_popcount(n)<<"\n";
+	}
+	return 0;
+}
